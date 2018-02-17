b8475f06c654321cb8b8092cc8e728f49e0dd333
teffalump <teffalump@riseup.net>
2018-02-17 10:01:43 -0500

    Update imports

--- health_fhir/__init__.py
+++ health_fhir/__init__.py
@@ -1,1 +1,2 @@
 from .adapters import *
+from .interface import *
--- health_fhir/interface/__init__.py
+++ health_fhir/interface/__init__.py
@@ -3,3 +3,4 @@ from .procedure_interface import *
 from .patient_interface import *
 from .practitioner_interface import *
 from .observation_interface import *
+from .family_member_history import *

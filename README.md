c5122761a3321de56590057a1a2ea86cab1cb0af
teffalump <teffalump@riseup.net>
2018-03-16 14:19:17 -0400

    Update README; Add custom exceptions

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
+#### Introduction
+
 This package aims to provide a functional FHIR interface to GNU Health data models. With the provided classes, one should be able to work with the back-end data from GNU Health in a FHIR-compatible way (through fhirclient).
 
 #### Design
 
-For every appropriate FHIR Resource, there will be an adapter which subclasses the fhirclient data model. This subclass takes the provided tryton model and imports the data. Basically, this is glue for fhirclient and GNU Health.
+The ultimate goal is that for every appropriate FHIR Resource there will be an adapter which subclasses the fhirclient data model. This subclass takes the provided tryton model and imports the data. Basically, this is glue for fhirclient and GNU Health.
 
 #### Resources
 
---
+++ health_fhir/exceptions/exception.py
@@ -0,0 +1,26 @@
+class GNUHealthError(Exception):
+    """
+    Base GNU Health exception
+    """
+
+class GNUHealthTypeError(GNUHealthError):
+    """
+    Exception raised when there is a type mismatch
+    """
+
+    def __init__(self, adapter, model, msg=None):
+        if msg is None:
+            msg = "{0} adapter cannot process this {1} model".format(adapter, model)
+        super(GNUHealthTypeError, self).__init__(msg)
+
+class GNUHealthImportError(GNUHealthError):
+    """
+    Exception raised when error importing data from model
+    """
+
+    def __init__(self, attribute, msg=None):
+        if msg is None:
+            msg = "Error importing {0} data from the model".format(attribute)
+        super(GNUHealthImportError, self).__init__(msg)
+
+__all__=['GNUHealthError', 'GNUHealthTypeError', 'GNUHealthImportError']

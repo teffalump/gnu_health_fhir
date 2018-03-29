82b298eda352daa8d646ab0cec322d7a6b7dde6c
teffalump <chris@teffalump.com>
2018-03-29 19:09:25 -0400

    Add some possibly useful valuesets

--- health_fhir/adapters/value_sets/__init__.py
+++ health_fhir/adapters/value_sets/__init__.py
@@ -3,3 +3,5 @@ from .admin_gender import *
 from .marital_status import *
 from .immunization_route import *
 from .immunization_site import *
+from .act_encounter_code import *
+from .encounter_status import *
